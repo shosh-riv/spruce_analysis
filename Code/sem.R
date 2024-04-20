0ab38c52265ff1c64b9be6fa487ce7afa8e230ce
Linnea <linneacsmith08@gmail.com>
2024-04-20 16:55:15 -0400

    save cleaned full dataset with precip

--- Code/sem.R
+++ Code/sem.R
@@ -1,6 +1,6 @@
 ### SEM for all SPRUCE variables ###
 
-spruce_orig <- read.csv("./Data/Clean/complete_combined_spruce_data.csv")
+spruce_orig <- read.csv("./Data/Clean/complete_combined_spruce_data_noprecip.csv")
 str(spruce_orig)
 
 # Expect bacteria and archaea copy num to be influenced by measured temp, depth,
@@ -26,6 +26,14 @@ spruce <- merge(spruce_orig,env[,c("Sample_date","Plot","PREC_6")],
 spruce[which(spruce$MBN<0),"MBN"] <- 0
 spruce[which(spruce$MBC<0),"MBC"] <- 0
 
+# Set MBN outlier to NA
+spruce[which(spruce$MBN>100),"MBN"] <- NA
+
+# Remove extra column
+spruce$X <- NULL
+
+# Save this new dataset
+write.csv(spruce,"./Data/Clean/complete_combined_spruce_data.csv",row.names=F)
 
 #### Covariance matrix ####
 # First, investigate correlations
@@ -314,15 +322,18 @@ spruce_num$Sample_date <- as.numeric(spruce_num$Sample_date)
 #### Log-transform and scale numeric data ####
 spruce_num_log_scale <- spruce_num
 
-### Log-transformation##spruce_num_log_scale# Log-transformation
+# Variables to transform
 to_transform <- c("Bacteria_copy_dry","Archaea_copy_dry","MBN","MBC")
-spruce_num_log_scale[,to_transform] <- log(spruce_num_log_scale[,to_transform])
 
-# Due to "heterogeneity between the two replicate samples" used to calculate MBN
-# and MBC, there are a few negative values that naturally become NAs when log-transformed.
-# Since negative values are nonsensical, having NAs for those values is desired behavior.
-# Another option would be to change these values to 0 or to a very small value,
-# to reflect that they have a very small microbial biomass.
+# Check for 0s
+apply(spruce_num_log_scale[,to_transform],2,FUN=min,na.rm=T)
+
+# Add 1 to MBC and MBN because they include 0
+spruce_num_log_scale$MBN <- spruce_num_log_scale$MBN + 1
+spruce_num_log_scale$MBC <- spruce_num_log_scale$MBC + 1
+
+# Log-transformation
+spruce_num_log_scale[,to_transform] <- log(spruce_num_log_scale[,to_transform])
 
 ### Scale all numerical values
 # Vector of numeric columns
@@ -344,16 +355,43 @@ spruce_num_log_scale[,to_scale] <- scale(spruce_num_log_scale[,to_scale])
 # Remove NA rows
 spruce_num_log_scale_noNA <- na.omit(spruce_num_log_scale)
 
+## Try fitting the same model with numerical data
+spruce_num_psem <- psem(
+  
+  # Intermediate layer: DOC, DN, temperature, GWC
+  lm(DOC_unfumigated_soil ~ depth2 + Temp_experimental + CO2_treatment + GWC, data=spruce_num_log_scale_noNA, na.action=na.omit),
+  lm(DN_unfumigated_soil ~ depth2 + Temp_experimental + CO2_treatment + GWC, data=spruce_num_log_scale_noNA, na.action=na.omit),
+  lm(temp ~ Temp_experimental + CO2_treatment + depth2,
+     data=spruce_num_log_scale_noNA, na.action=na.omit),
+  lm(GWC ~ depth2 + temp + PREC_6, data=spruce_num_log_scale_noNA, na.action=na.omit),
+  
+  # Predicted variables layer: Bacteria and Archaea copy numbers, MBN, MBC
+  lm(Bacteria_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+     data=spruce_num_log_scale_noNA, na.action=na.omit),
+  lm(Archaea_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+     data=spruce_num_log_scale_noNA, na.action=na.omit),
+  lm(MBN ~ DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+     data=spruce_num_log_scale_noNA, na.action=na.omit),
+  lm(MBC ~ DOC_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+     data=spruce_num_log_scale_noNA, na.action=na.omit)
+  
+)
+
+summary(spruce_num_psem, .progressBar = FALSE)
+# This is the worst model we've fit yet by far (bad R2s). Compare AICs:
+AIC_psem(spruce_psem)
+AIC_psem(spruce_num_psem)
+# Numeric is worse than categorical; stick to categorical dataset.
 
 ## Model identification
-# Model still doesn't fit. Check the t-rule. It currently includes
+# Model still doesn't fit. Check the t-rule. The model currently includes
 # 13 variables; according to the t-rule, t <= [n(n+1)]/2, where n is the number
 # of unique cells in the variance-covariance matrix (the diagonal and the lower
 # off-diagonal; see: https://jslefche.github.io/sem_book/global-estimation.html#model-identifiability)
 # With 13 variables, n=91. 
 (91*(91+1))/2
 
-# We have 51 relationships and 13 parameters to estimate, so t = 51+13 = 64. This
+# We have 34 relationships and 13 parameters to estimate, so t = 51+13 = 64. This
 # is valid, assuming I'm using the t-rule right. We move on to checking sample size.
 
 # "The basic rule-of-thumb is that the level of replication should be at least 5 times the 
@@ -363,34 +401,10 @@ spruce_num_log_scale_noNA <- na.omit(spruce_num_log_scale)
 # the number of estimated parameters. "
 # We're estimating 51 relationships with our current model structure.
 51*5
-# This is 255, which is a few more than our 214 samples.
-214/5
-# At the most, we can include 42 relationships; ideally, we include fewer than this.
+# This is 255, which is more than our 177 samples.
+177/5
+# At the most, we can include 35 relationships; ideally, we include fewer than this.
 
-## Try fitting a new model
-spruce_psem <- psem(
-  
-  # Intermediate layer: DOC, DN, temperature, GWC
-  lm(DOC_unfumigated_soil ~ depth2 + CO2_treatment + GWC, data=spruce_log_scale, na.action=na.omit),
-  lm(DN_unfumigated_soil ~ depth2 + CO2_treatment + GWC, data=spruce_log_scale, na.action=na.omit),
-  lm(temp ~ Temp_experimental + CO2_treatment + depth2,
-     data=spruce_log_scale, na.action=na.omit),
-  lm(GWC ~ depth2 + temp + PREC_6, data=spruce_log_scale, na.action=na.omit),
-  
-  # Predicted variables layer: Bacteria and Archaea copy numbers, MBN, MBC
-  lm(Bacteria_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
-     data=spruce_log_scale, na.action=na.omit),
-  lm(Archaea_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
-     data=spruce_log_scale, na.action=na.omit),
-  lm(MBN ~ DN_unfumigated_soil + temp + GWC + depth2,
-     data=spruce_log_scale, na.action=na.omit),
-  lm(MBC ~ DOC_unfumigated_soil + temp + GWC + depth2,
-     data=spruce_log_scale, na.action=na.omit)
-  
-)
-
-summary(spruce_psem, .progressBar = FALSE)
-# This is the worst model we've fit yet by far (bad R2s)
 
 ## What if we do one SEM per response variable to bring down the total number of relationships?
 
