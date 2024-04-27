545e82f2743fb43fda3208fa436ea48217329668
Linnea <linneacsmith08@gmail.com>
2024-04-27 18:31:31 -0400

    play around with formulas

--- Code/anovas.R
+++ Code/anovas.R
@@ -30,17 +30,34 @@ length(response_vars) * 7 # 7 because each response can be run against one of th
 # ANOVAs for each response variable.
 
 # Select variable for ANOVA
-lhs <- "Bacteria_copy_dry"
+lhs <- "Archaea_copy_dry"
 
 # Select explantory variable(s)
 x_axis <- "depth2"
 fill_var <- "Temp_experimental"
 
+# Remove ambient temp
+spruce_noamb <- subset(spruce,Temp_experimental != "Amb")
+
+# Log-transform response variables
+spruce_noamb_log <- spruce_noamb
+to_transform <- c("Bacteria_copy_dry","Archaea_copy_dry","MBN","MBC")
+
+# Check for 0s
+apply(spruce_noamb_log[,to_transform],2,FUN=min,na.rm=T)
+
+# Add 1 to MBC and MBN because they include 0
+spruce_noamb_log$MBN <- spruce_noamb_log$MBN + 1
+spruce_noamb_log$MBC <- spruce_noamb_log$MBC + 1
+
+# Transform
+spruce_noamb_log[,to_transform] <- log(spruce_noamb_log[,to_transform])
+
 # Create formula
 aov_formula <- paste0(lhs,"~",paste(x_axis,fill_var,sep ="*"))
 
 # Run ANOVA
-aov_object <- aov(formula(aov_formula), data=spruce)
+aov_object <- aov(formula(aov_formula), data=spruce_noamb_log)
 
 # Run TukeyHSD
 tukey <- TukeyHSD(aov_object)
@@ -72,12 +89,16 @@ if(length(int) > 0){
 x_letters <- multcompLetters(tukey_p[[x_axis]])$Letters
 x_letters <- data.frame(level = names(x_letters),letter = x_letters)
 
-fill_letters <- multcompLetters(tukey_p[[fill_var]])
+fill_letters <- multcompLetters(tukey_p[[fill_var]])$Letters
+
+for_legend <- paste(names(fill_letters)," (",fill_letters,")",sep="")
+names(for_legend) <- names(fill_letters)
 
 # Boxplot
 library(ggplot2)
-ggplot(data=spruce)+
+ggplot(data=spruce_noamb_log)+
   geom_boxplot(aes(x=.data[[x_axis]],y=.data[[lhs]],fill=.data[[fill_var]]))+
   theme_classic() +
-  geom_text(data=x_letters,aes(x=level,y=(max(spruce[[lhs]])+max(spruce[[lhs]])/10)),label=x_letters$letter)
+  geom_text(data=x_letters,aes(x=level,y=(max(spruce_noamb_log[[lhs]])+max(spruce_noamb_log[[lhs]])/10)),label=x_letters$letter) +
+  scale_fill_discrete(labels=for_legend)
 
