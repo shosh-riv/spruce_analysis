bf150733d1f451165e289756a8e47973751d06e8
Linnea <linneacsmith08@gmail.com>
2024-05-01 00:55:40 -0400

    Modify lm, SEM to reflect initial hypotheses

--- Code/sem.R
+++ Code/sem.R
@@ -64,127 +64,122 @@ library("lme4")
 # date as a fixed effect?
 
 #### Testing model assumptions ####
+### For all, use the dataset without the ambient temperature treatment
 
 ## Dissolved organic carbon
-DOC_lm <- lm(DOC_unfumigated_soil ~ depth2, data=spruce, na.action=na.omit)
+DOC_lm <- lm(DOC_unfumigated_soil ~ depth2 + CO2_treatment + depth2, data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(DOC_lm)
 # Mostly fine, little bit of a right skew but I think it's good enough
 # Check if the DOC is right-skewed
 par(mfrow=c(1,2))
-hist(spruce$DOC_unfumigated_soil)
+hist(spruce_noAmb$DOC_unfumigated_soil)
 
 # Does it look better log-transformed?
-hist(log(spruce$DOC_unfumigated_soil))
+hist(log(spruce_noAmb$DOC_unfumigated_soil))
 # It does. Does transformation improve the Q-Q plot?
-DOC_log_lm <- lm(log(DOC_unfumigated_soil) ~ depth2 + Sample_date, data=spruce, na.action=na.omit)
+DOC_log_lm <- lm(log(DOC_unfumigated_soil) ~ depth2 + CO2_treatment + depth2, data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(DOC_log_lm)
 # Somewhat, but I don't know that it's enough to be worth it.
 
 ## Dissolved nitrogen
-DN_lm <- lm(DN_unfumigated_soil ~ depth2, data=spruce, na.action=na.omit)
+DN_lm <- lm(DN_unfumigated_soil ~ depth2 + Temp_experimental, data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(DN_lm)
 # Same as DOC. Check histograms
 par(mfrow=c(1,2))
-hist(spruce$DN_unfumigated_soil)
-hist(log(spruce$DN_unfumigated_soil))
+hist(spruce_noAmb$DN_unfumigated_soil)
+hist(log(spruce_noAmb$DN_unfumigated_soil))
 # Here, log-transforming actually leads to more of a left-skew than otherwise
 
 ## Temperature
-temp_lm <- lm(temp ~ Temp_experimental + CO2_treatment + depth2 + Sample_date,
-              data=spruce, na.action=na.omit)
+temp_lm <- lm(temp ~ Temp_experimental + depth2,
+              data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(temp_lm)
 # Also fine, check histograms for completeness' sake
 par(mfrow=c(1,2))
-hist(spruce$temp)
-hist(log(spruce$temp))
+hist(spruce_noAmb$temp)
+hist(log(spruce_noAmb$temp))
 # Untransformed definitely better
 
 ## Gravimetric water content
-GWC_lm <- lm(GWC ~ depth2 + PREC_6 + Sample_date, data=spruce, na.action=na.omit)
+GWC_lm <- lm(GWC ~ depth2 + PREC_6, data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(GWC_lm)
 # Mostly fine but definitely a skew in the Q-Q plot
 par(mfrow=c(1,2))
-hist(spruce$GWC)
-hist(log(spruce$GWC))
+hist(spruce_noAmb$GWC)
+hist(log(spruce_noAmb$GWC))
 # I'm not sure that log-transformming is the move here. Does it change the Q-Q plot?
-GWC_log_lm <- lm(log(GWC) ~ depth2 + PREC_6 + Sample_date, data=spruce, na.action=na.omit)
+GWC_log_lm <- lm(log(GWC) ~ depth2 + PREC_6, data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(GWC_log_lm)
 # It actually skews it worse. Stick to the untransformed data.
 
 ## Bacteria copy number
-BCN_lm <- lm(Bacteria_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp,
-             data=spruce, na.action=na.omit)
+BCN_lm <- lm(Bacteria_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC,
+             data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(BCN_lm)
 # Heavy skew
 par(mfrow=c(1,2))
-hist(spruce$Bacteria_copy_dry)
-hist(log(spruce$Bacteria_copy_dry))
+hist(spruce_noAmb$Bacteria_copy_dry)
+hist(log(spruce_noAmb$Bacteria_copy_dry))
 # log-transformation seems to improve things considerably
-BCN_log_lm <- lm(log(Bacteria_copy_dry) ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp,
-                 data=spruce, na.action=na.omit)
+BCN_log_lm <- lm(log(Bacteria_copy_dry) ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC,
+                 data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(BCN_log_lm)
 # This is much more respectable. We may have to remove a couple outliers, though.
 # Check points 135 and 108: 
-spruce[c(108,135),]
+spruce_noAmb[c(108,135),]
 # Keep these in mind while going through the rest.
 
 ## Archaea copy number
-ACN_lm <- lm(Archaea_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp,
-             data=spruce, na.action=na.omit)
+ACN_lm <- lm(Archaea_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC,
+             data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(ACN_lm)
 # Same skewed behavior as bacteria
 par(mfrow=c(1,2))
-hist(spruce$Archaea_copy_dry)
-hist(log(spruce$Archaea_copy_dry))
+hist(spruce_noAmb$Archaea_copy_dry)
+hist(log(spruce_noAmb$Archaea_copy_dry))
 # log-transformation seems to improve things considerably
-ACN_log_lm <- lm(log(Archaea_copy_dry) ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp,
-                 data=spruce, na.action=na.omit)
+ACN_log_lm <- lm(log(Archaea_copy_dry) ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC,
+                 data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(ACN_log_lm)
 # This is also better, and the points that were outliers for bacteria aren't here.
 
 ## Microbial biomass nitrogen
 MBN_lm <- lm(MBN ~ DN_unfumigated_soil + temp + GWC,
-             data=spruce, na.action=na.omit)
+             data=spruce_noAmb, na.action=na.omit)
 plot(MBN_lm)
-# Point 139 is a huge outlier, we noticed it in the covariance testing as well.
-# Remove it by setting it to NA but keeping the row intact (is this legit?)
-spruce_no_out <- spruce
-#spruce_no_out[139,"MBN"] <- NA
-MBN_lm <- lm(MBN ~ DN_unfumigated_soil + temp + GWC,
-             data=spruce_no_out, na.action=na.omit)
-plot(MBN_lm)
-# Looking better, but very skewed.
+
+# Skewed, log-transform (add 1 because there are 0s in the dataset)
 par(mfrow=c(1,2))
-hist(spruce_no_out$MBN)
-hist(log(spruce_no_out$MBN))
-# Looks nicer, let's test a new model.
-MBN_log_lm <- lm(log(MBN) ~ DN_unfumigated_soil + temp + GWC,
-                 data=spruce_no_out, na.action=na.omit)
+hist(spruce_noAmb$MBN)
+hist(log(spruce_noAmb$MBN+1))
+# This actually doesn't help matters all that much...
+MBN_log_lm <- lm(log(MBN+1) ~ DN_unfumigated_soil + temp + GWC,
+                 data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(MBN_log_lm)
-# Very nice!
+# A little nicer
 
 ## Microbial biomass carbon
 MBC_lm <- lm(MBC ~ DOC_unfumigated_soil + temp + GWC,
-             data=spruce, na.action=na.omit)
+             data=spruce_noAmb, na.action=na.omit)
 plot(MBC_lm)
 # Also somewhat skewed, not as bad as the others but probably worth log-transforming
 par(mfrow=c(1,2))
-hist(spruce$MBC)
-hist(log(spruce$MBC))
+hist(spruce_noAmb$MBC)
+hist(log(spruce_noAmb$MBC+1))
 # It just kind of shifts to left-skewed... still, see if this helps the model
-MBC_log_lm <- lm(log(MBC) ~ DOC_unfumigated_soil + temp + GWC,
-                 data=spruce, na.action=na.omit)
+MBC_log_lm <- lm(log(MBC+1) ~ DOC_unfumigated_soil + temp + GWC,
+                 data=spruce_noAmb, na.action=na.omit)
 par(mfrow=c(2,2))
 plot(MBC_log_lm)
 # Honestly not sure which is better! Maybe go with log-transformed for consistency. 
@@ -262,27 +257,42 @@ library(piecewiseSEM)
 spruce_psem <- psem(
   
   # Intermediate layer: DOC, DN, temperature, GWC
-  lm(DOC_unfumigated_soil ~ depth2 + Temp_experimental + CO2_treatment + GWC, data=spruce_log_scale, na.action=na.omit),
-  lm(DN_unfumigated_soil ~ depth2 + Temp_experimental + CO2_treatment + GWC, data=spruce_log_scale, na.action=na.omit),
-  lm(temp ~ Temp_experimental + CO2_treatment + depth2,
+  lm(DOC_unfumigated_soil ~ depth2 + CO2_treatment, data=spruce_log_scale, na.action=na.omit),
+  lm(DN_unfumigated_soil ~ depth2 + Temp_experimental, data=spruce_log_scale, na.action=na.omit),
+  lm(temp ~ Temp_experimental + depth2,
      data=spruce_log_scale, na.action=na.omit),
-  lm(GWC ~ depth2 + temp + PREC_6, data=spruce_log_scale, na.action=na.omit),
+  lm(GWC ~ depth2 + PREC_6, data=spruce_log_scale, na.action=na.omit),
   
   # Predicted variables layer: Bacteria and Archaea copy numbers, MBN, MBC
-  lm(Bacteria_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+  lm(Bacteria_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC,
      data=spruce_log_scale, na.action=na.omit),
-  lm(Archaea_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+  lm(Archaea_copy_dry ~ DOC_unfumigated_soil + DN_unfumigated_soil + temp + GWC,
      data=spruce_log_scale, na.action=na.omit),
-  lm(MBN ~ DN_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+  lm(MBN ~ DN_unfumigated_soil + temp + GWC,
      data=spruce_log_scale, na.action=na.omit),
-  lm(MBC ~ DOC_unfumigated_soil + temp + GWC + depth2 + Temp_experimental,
+  lm(MBC ~ DOC_unfumigated_soil + temp + GWC,
      data=spruce_log_scale, na.action=na.omit)
   
 )
 
 summary(spruce_psem, .progressBar = FALSE)
 
-## What happens if we make all variables numeric?
+## Add some parameters in based on d-sep tests (currently can afford to add 12 relationships)
+
+# Add depth to MBN, MBC (10 relationships left to add)
+spruce_psem <- update(spruce_psem,
+       MBN ~ DN_unfumigated_soil + temp + GWC + depth2,
+       MBC ~ DOC_unfumigated_soil + temp + GWC + depth2)
+summary(spruce_psem)
+# R-squared for MBN and MBC much higher now!
+
+# Add temperature to DOC, DN
+spruce_psem <- update(spruce_psem,
+                      DOC_unfumigated_soil ~ depth2 + CO2_treatment + temp,
+                      DN_unfumigated_soil ~ depth2 + temp)
+summary(spruce_psem)
+
+## What happens if we make all variables numeric?## WTemp_experimentalhat happens if we make all variables numeric?
 
 #### Make categorical variables numeric ####
 spruce_num <- spruce_noAmb
